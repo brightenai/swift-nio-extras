e1bf67e7547ab84da4f4ef8bc49bdf34741c8115
john burkey <johnburkey@brighten.ai>
2020-08-17 18:11:58 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
         .library(name: "NIOHTTPCompression", targets: ["NIOHTTPCompression"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.9.0"),
+        .package(url: "https://github.com/apple/swift-nio.git",.branch("master")),// from: "2.9.0"),
     ],
     targets: targets
 )
